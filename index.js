7a26692e53fe5984013ccbf1005f34449f733e53
jeremybouldin <56416889+jeremybouldin@users.noreply.github.com>
2022-12-09 13:49:48 -0500

    Render tweets

--- index.js
+++ index.js
@@ -1,7 +1,50 @@
 import { tweetsData } from "./data.js";
 const tweetInput = document.getElementById('tweet-input')
 const tweetBtn = document.getElementById('tweet-btn')
+const feedArea = document.getElementById('feed')
 
 tweetBtn.addEventListener('click', function(){
     console.log(tweetInput.value)
+})
+
+function getFeedHtml(tweets) {
+    let feedHtml = ''
+    tweets.forEach(function(tweet){
+        feedHtml += `
+        <div class="tweet">
+            <div class="tweet-inner">
+                <img src="${tweet.profilePic}" class="profile-pic">
+                <div>
+                    <p class="handle">${tweet.handle}</p>
+                    <p class="tweet-text">${tweet.tweetText}</p>
+                    <div class="tweet-details">
+                        <span class="tweet-detail">
+                            <i class="fa-regular fa-comment-dots" data-reply="${tweet.uuid}"></i>
+                            ${tweet.replies.length}
+                        </span>
+                        <span class="tweet-detail">
+                            <i class="fa-solid fa-heart" data-like="${tweet.uuid}"></i>
+                            ${tweet.likes}
+                        </span>
+                        <span class="tweet-detail">
+                            <i class="fa-solid fa-retweet" data-retweet="${tweet.uuid}"></i>
+                            ${tweet.retweets}
+                        </span>
+                    </div>   
+                </div>            
+            </div>
+        </div>
+        `
+    })
+    return feedHtml
+}
+
+function renderTweets(){
+    feedArea.innerHTML = getFeedHtml(tweetsData)
+}
+
+renderTweets()
+
+document.addEventListener('click', function(e){
+    console.log(e.target.dataset)
 })
